151b3ffc9353ace42a22149ce80ec41700b86a2e
jonaseberle <jonaseberle@bd1a39f8-fc1d-4aa7-9ee9-936774de877b>
2011-02-08 15:30:45 +0000

    Unix shell script to find libraries (as suggested in http://doc.qt.nokia.com/latest/deployment-x11.html#linking-the-application-to-qt-as-a-shared-library)

---
+++ trunk/QuteScoop/QuteScoop.sh
@@ -0,0 +1,14 @@
+ #!/bin/sh
+ appname=`basename $0 | sed s,\.sh$,,`
+
+ dirname=`dirname $0`
+ tmp="${dirname#?}"
+
+ if [ "${dirname%$tmp}" != "/" ]; then
+ dirname=$PWD/$dirname
+ fi
+ LD_LIBRARY_PATH=$dirname
+ export LD_LIBRARY_PATH
+ $dirname/$appname $*
+
+# http://doc.qt.nokia.com/latest/deployment-x11.html#linking-the-application-to-qt-as-a-shared-library
