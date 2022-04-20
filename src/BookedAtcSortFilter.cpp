4b0f5dd26569a7cdcd1c3dc097b79383dd47c138
Jonas Eberle <flightvision@googlemail.com>
2022-04-20 19:42:15 +0200

    [BUGFIX] Fix several issues with Booked Controllers dialog
    This fixes a non-working from-to time window filter and some timezone
    conversion problems.
    
    As a drive-by change it introduces proper sorting on date/time columns.

--- src/BookedAtcSortFilter.cpp
+++ src/BookedAtcSortFilter.cpp
@@ -4,33 +4,41 @@
 
 #include "BookedAtcSortFilter.h"
 
-#include "BookedAtcDialog.h"
-
-bool BookedAtcSortFilter::filterAcceptsRow(int source_row, const QModelIndex& source_parent) const {
-    if (!this->_from.isNull() && !this->_to.isNull()) {
+bool BookedAtcSortFilter::filterAcceptsRow(int source_row, const QModelIndex& source_parent) const
+{
+    if (this->_from.isValid() && this->_to.isValid()) {
         if (sourceModel()->index(source_row, 0, source_parent).isValid()) {
-            QDateTime starts =
-                    sourceModel()->index(source_row, 5, source_parent)
-                    .data(Qt::EditRole).toDateTime();
-            QDateTime ends =
-                    sourceModel()->index(source_row, 6, source_parent)
-                    .data(Qt::EditRole).toDateTime();
-            if (_to == _from && _from < ends) return
-                    (QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent));
-            if (_from < starts && _to > starts) return
-                    (QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent));
-            if (_from < ends && _to > starts) return
-                    (QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent));
-            return (false);
+            QDateTime starts = sourceModel()->index(source_row, 4, source_parent).data(Qt::EditRole).toDateTime().toUTC();
+            QDateTime ends = sourceModel()->index(source_row, 5, source_parent).data(Qt::EditRole).toDateTime().toUTC();
+            if (
+                (_to == _from && _from <= ends) // _to == _from means for: ever
+                || (_from <= starts && _to >= starts)
+                || (_from <= ends && _to >= starts)
+            ) {
+                return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent);
+            }
+            return false;
         }
     } else
-        return(QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent));
-    return (false);
+        return QSortFilterProxyModel::filterAcceptsRow(source_row, source_parent);
+    return false;
 }
 
-void BookedAtcSortFilter::setDateTimeRange(QDateTime& dtfrom, QDateTime& dtto) {
-    this->_from = dtfrom;
-    this->_to = dtto;
+bool BookedAtcSortFilter::lessThan(const QModelIndex &left, const QModelIndex &right) const
+{
+    auto leftData = sourceModel()->data(left, Qt::EditRole);
+    auto rightData = sourceModel()->data(right, Qt::EditRole);
+    QTextStream(stdout) << leftData.toString() << "\n";
+
+    if (leftData.userType() == QMetaType::QDateTime) {
+        return leftData.toDateTime() < rightData.toDateTime();
+    }
+    return QString::localeAwareCompare(leftData.toString(), rightData.toString()) < 0;
+}
+
+void BookedAtcSortFilter::setDateTimeRange(QDateTime& from, QDateTime& to)
+{
+    this->_from = from;
+    this->_to = to;
     this->invalidateFilter();
 }
-
